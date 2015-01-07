13654329935f5b2f3f7c7384528c01d1041b076c
Jesse Pollak <jpollak92@gmail.com>
2015-01-07 19:32:10 -0400

    splits User model out into a separate file

---
+++ models/user.rb
@@ -0,0 +1,14 @@
+require 'data_mapper'
+
+DataMapper.setup(:default, 'sqlite::memory:')
+
+class User
+    include DataMapper::Resource
+
+    property :id,         Serial        # An auto-increment integer key
+    property :email,      String        # The user's email
+    property :clef_id,    Integer       # The user's Clef ID
+    property :logged_out_at, Integer    # When the user was last logged out by Clef
+end
+
+DataMapper.auto_upgrade!
