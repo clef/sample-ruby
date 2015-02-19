dadb0712346477146bd5e50ed55b1a197e7a2d39
Jesse Pollak <jpollak92@gmail.com>
2015-02-18 20:56:42 -0800

    change to two space indents

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ require_relative 'models/user'
 # Configuration for the application
 #
 configure do
-    enable :sessions, :logging
-    set :session_secret, 'REPLACE THIS IN YOUR APP'
-    set :clef_api_base, 'https://clef.io/api/v1'
-    set :clef_app_id, '58247f018c3fdac32abdacddfcfaf8fc'
-    set :clef_app_secret, '2ef51ea751ae2bb36ffdb2a63016c6bb'
+  enable :sessions, :logging
+  set :session_secret, 'REPLACE THIS IN YOUR APP'
+  set :clef_api_base, 'https://clef.io/api/v1'
+  set :clef_app_id, '58247f018c3fdac32abdacddfcfaf8fc'
+  set :clef_app_secret, '2ef51ea751ae2bb36ffdb2a63016c6bb'
 end
 
 ##
@@ -21,20 +21,20 @@ end
 # Read more about how this works here: http://docs.getclef.com/v1.0/docs/overview
 #
 before do
-    if session[:user] and @user = User.get(session[:user])
-        if session[:logged_in_at] and @user.logged_out_at and @user.logged_out_at > session[:logged_in_at]
-            @user = nil
-            session.delete(:logged_in_at)
-            session.delete(:user)
-        end
+  if session[:user] and @user = User.get(session[:user])
+    if session[:logged_in_at] and @user.logged_out_at and @user.logged_out_at > session[:logged_in_at]
+      @user = nil
+      session.delete(:logged_in_at)
+      session.delete(:user)
     end
+  end
 end
 
 ##
 # Render the index template which either shows the Clef button or user information
 #
 get '/' do
-    erb :index
+  erb :index
 end
 
 ##
@@ -43,47 +43,47 @@ end
 # Read more here: http://docs.getclef.com/v1.0/docs/authenticating-users
 #
 get '/callback/login' do
-    return redirect to('/') if @user
+  return redirect to('/') if @user
 
-    code = params[:code]
-    data = {
-        body: {
-            code: code,
-            app_id: settings.clef_app_id,
-            app_secret: settings.clef_app_secret
-        }
+  code = params[:code]
+  data = {
+    body: {
+      code: code,
+      app_id: settings.clef_app_id,
+      app_secret: settings.clef_app_secret
     }
+  }
 
-    response = HTTParty.post("#{settings.clef_api_base}/authorize", data)
+  response = HTTParty.post("#{settings.clef_api_base}/authorize", data)
+
+  if response['success']
+    access_token = response['access_token']
+
+    url = "#{settings.clef_api_base}/info?access_token=#{access_token}"
+    response = HTTParty.get(url)
 
     if response['success']
-        access_token = response['access_token']
 
-        url = "#{settings.clef_api_base}/info?access_token=#{access_token}"
-        response = HTTParty.get(url)
+      info = response['info']
+      unless @user = User.first(clef_id: info['id'])
+        @user = User.create(
+          email: info['email'],
+          clef_id: info['id']
+        )
+      end
 
-        if response['success']
+      session[:logged_in_at] = Time.now.to_i
+      session[:user] = @user.id
 
-            info = response['info']
-            unless @user = User.first(clef_id: info['id'])
-                @user = User.create(
-                    email: info['email'],
-                    clef_id: info['id']
-                )
-            end
-
-            session[:logged_in_at] = Time.now.to_i
-            session[:user] = @user.id
-
-            redirect to('/')
-        else
-            status 500
-            response['error']
-        end
+      redirect to('/')
     else
-        status 500
-        response['error']
+      status 500
+      response['error']
     end
+  else
+    status 500
+    response['error']
+  end
 end
 
 ##
@@ -92,24 +92,24 @@ end
 # Read more here: http://docs.getclef.com/v1.0/docs/handling-the-logout-webhook
 #
 post '/callback/logout' do
-    content_type :json
+  content_type :json
 
-    data = {
-        body: {
-            logout_token: params[:logout_token],
-            app_id: settings.clef_app_id,
-            app_secret: settings.clef_app_secret
-        }
+  data = {
+    body: {
+      logout_token: params[:logout_token],
+      app_id: settings.clef_app_id,
+      app_secret: settings.clef_app_secret
     }
+  }
 
-    response = HTTParty.post("#{settings.clef_api_base}/logout", data)
+  response = HTTParty.post("#{settings.clef_api_base}/logout", data)
 
-    if response['success']
-        user = User.first(clef_id: response['clef_id'])
-        user.update(logged_out_at: Time.now.to_i)
-        { success: true }.to_json
-    else
-        status 500
-        { error: response['error'] }.to_json
-    end
+  if response['success']
+    user = User.first(clef_id: response['clef_id'])
+    user.update(logged_out_at: Time.now.to_i)
+    { success: true }.to_json
+  else
+    status 500
+    { error: response['error'] }.to_json
+  end
 end
--- models/user.rb
+++ models/user.rb
@@ -3,12 +3,12 @@ require 'data_mapper'
 DataMapper.setup(:default, 'sqlite::memory:')
 
 class User
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,         Serial        # An auto-increment integer key
-    property :email,      String        # The user's email
-    property :clef_id,    Integer       # The user's Clef ID
-    property :logged_out_at, Integer    # When the user was last logged out by Clef
+  property :id,         Serial        # An auto-increment integer key
+  property :email,      String        # The user's email
+  property :clef_id,    Integer       # The user's Clef ID
+  property :logged_out_at, Integer    # When the user was last logged out by Clef
 end
 
 DataMapper.auto_upgrade!
